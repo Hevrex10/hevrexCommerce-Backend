151e8dca8f949e410450133e15924dbfba0a8603
hevrex10 <eadeagbo110@gmail.com>
2026-09-18 10:26:57 +0100

    testing email transporter

--- utils/email.js
+++ utils/email.js
@@ -1,10 +1,17 @@
 import nodemailer from "nodemailer";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 dotenv.config({ path: "./config.env" });
 
-
 export async function sendEmail(options) {
+  console.log("EMAIL CONFIG:", {
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    username: process.env.EMAIL_USERNAME,
+    passwordExists: !!process.env.EMAIL_PASSWORD,
+    from: process.env.EMAIL_FROM,
+  });
+
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
