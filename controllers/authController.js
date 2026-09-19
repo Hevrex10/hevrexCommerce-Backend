00fa4dd30c0c581631f2584741a5d4a2d682b194
hevrex10 <eadeagbo110@gmail.com>
2026-09-19 12:47:21 +0100

    update jwt cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,6 +51,7 @@ export async function login(req, res, next) {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
